12967a1acdda80c79fafae7b14fdd5eea9147b77
Unknown <leo.zerino@gmail.com>
2019-03-27 19:48:28 -0300

    Mobile, var num e botoes
    Adicionado viewport, regras para css mobile, variravel numerica global para random color e botões do fullpage

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,62 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
 		licenseKey: "OPEN-SOURCE-GPLV3-LICENSE",
-		scrollingSpeed: 1500,
+        anchors: ['p1', 'p2', 'p3'],
+		scrollingSpeed: 1200,
+        controlArrows: true,
 	});
 
-    var color = randomColor({luminosity: 'light', count: 48});
+
+
+    //cor aleatoria
+    var num = 15;
+    var color = randomColor({luminosity: 'light', count: num});
  	$(".rback").each(function () {
-        var rand = Math.floor(Math.random() * 48) + 1
-        $(this).css("background-color",color[rand]);
+        var rand = Math.floor(Math.random() * num) + 1
+        $(this).css("background-color",color[rand]);''
     });
     $(".rtext").each(function () {
-        var rand = Math.floor(Math.random() * 48) + 1
+        var rand = Math.floor(Math.random() * num) + 1
         $(this).css("color",color[rand]);
     });
     $(".rborder").each(function () {
-        var rand = Math.floor(Math.random() * 48) + 1
+        var rand = Math.floor(Math.random() * num) + 1
         $(this).css("border-left-color",color[rand]);
     });
+
+
+
+    //botao home
+    $(document).on('click', '.home', function(){
+        fullpage_api.moveTo('p1');
+    });
+    //botao proximo
+     $(document).on('click', '.prox', function(){
+        fullpage_api.moveSectionDown();
+    });
+    //botao anterior
+     $(document).on('click', '.ante', function(){
+        fullpage_api.moveSectionUp();
+    });
+    //botao videos
+     $(document).on('click', '.vid', function(){
+        fullpage_api.moveSlideRight();
+    });
+    //botao voltar  videos
+     $(document).on('click', '.bvid', function(){
+        fullpage_api.moveSlideLeft();
+    });
+
+
+     $(".but").hover(
+        // mouse in
+        function(){
+            $(this).css("filter","invert(100%)");
+        },
+        // mouse out
+        function(){
+            $(this).css("filter","unset");
+        }
+
+    );
 });
